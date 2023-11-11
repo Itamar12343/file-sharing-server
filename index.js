c819c06e57baebbbf3c0a112f3a1a41706a52040
itamar hess <Itamardarihess@gmail.com>
2023-11-11 18:37:06 +0200

    socket communication

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const { Socket } = require("socket.io");
-
 const io = require("socket.io")(3000, {
     cors: {
         origin: "*"
     }
 });
 
-io.on("connection", ws => {
+let nameList = [];
 
+
+io.on("connection", socket => {
+    socket.on("add-name-to-list", name => {
+        nameList.push(name);
+        console.log(nameList);
+    });
 });
