47470337deb4757cb7c6c33f86cc3a8c31cfc4ff
itamar hess <Itamardarihess@gmail.com>
2023-11-25 22:01:26 +0200

    new update

--- index.js
+++ index.js
@@ -8,8 +8,38 @@ let nameList = [];
 
 
 io.on("connection", socket => {
-    socket.on("add-name-to-list", name => {
-        nameList.push(name);
+
+    socket.on("receiverConnect", name => {
+        addNameToList(name);
         console.log(nameList);
     });
+
 });
+
+
+function addNameToList(name) {
+
+    if (nameList[0] == undefined) {
+        nameList.push(name);
+    }
+
+    nameList.forEach(list => {
+        if (name != list) {
+            nameList.push(name);
+        } else {
+            nameList.push(name + " 1");
+        }
+    });
+
+}
+
+
+
+function removeNameFromList(name) {
+    let newArray = [...nameList];
+    const index = newArray.findIndex((element) => element == name);
+    if (index != -1) {
+        newArray.splice(index, 1);
+        nameList = [...newArray];
+    }
+}
