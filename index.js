f7ab6d48255dd7dae3234aba42e395faa80d656f
itamar hess <Itamardarihess@gmail.com>
2023-12-05 16:08:16 +0200

    improvements

--- index.js
+++ index.js
@@ -5,32 +5,57 @@ const io = require("socket.io")(3000, {
 });
 
 let nameList = [];
+let isDualName = false;
 
 
 io.on("connection", socket => {
 
     socket.on("receiverConnect", name => {
-        addNameToList(name);
-        console.log(nameList);
+        let myName;
+
+        if (checkIfNameAlredyExist(name) == "true") {
+            addNameToList(name);
+            myName = name;
+        } else {
+            console.log(false);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log("hi");
     });
 
 });
 
 
+
 function addNameToList(name) {
+
+    nameList.forEach(list => {
+        if (name != list) {
+            nameList.push(name);
+        }
+
+    });
 
     if (nameList[0] == undefined) {
         nameList.push(name);
     }
 
+}
+
+function checkIfNameAlredyExist(name) {
     nameList.forEach(list => {
         if (name != list) {
-            nameList.push(name);
+            return "true";
         } else {
-            nameList.push(name + " 1");
+            return "false";
         }
     });
 
+    if (nameList[0] == undefined) {
+        return "true";
+    }
 }
 
 
